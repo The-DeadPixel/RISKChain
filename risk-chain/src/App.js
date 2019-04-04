f9fa546f352c6bfd2e08b2998117aa7319afdde2
sgthun <sthunquest@gmail.com>
2019-04-04 16:05:18 -0600

    More controlls

--- risk-chain/src/App.js
+++ risk-chain/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       to: 'Select a Country',
       type: false,
       troops:'',
-      phase: '',
+      phase: 0,
       hilite: ''
     };
     this.selectCountry = this.selectCountry.bind(this);
@@ -22,6 +22,9 @@ class App extends Component {
     this.render = this.render.bind(this);
     this.getTestBoard = this.getTestBoard.bind(this);
     this.makeSelect = this.makeSelect.bind(this);
+    this.getSubmitButton = this.getSubmitButton.bind(this);
+    this.nextPhase = this.nextPhase.bind(this);
+    this.getControllTitle = this.getControllTitle.bind(this);
   }
 
   selectCountry(country) {
@@ -65,7 +68,7 @@ class App extends Component {
           },
           2:{
             owner: player3,
-            troops: 4
+            troops: 6
           },
           3:{
             owner: player3,
@@ -73,7 +76,7 @@ class App extends Component {
           },
           4:{
             owner: player2,
-            troops: 4
+            troops: 2
           },
           5:{
             owner: player1,
@@ -236,9 +239,9 @@ class App extends Component {
         }
       },
       config:{
-        turn: player1,
+        turn: player2,
         phase: 0,
-        opponents: [player2, player3]
+        opponents: [player1, player3]
 
       },
       card:{
@@ -277,28 +280,72 @@ class App extends Component {
       }
     }
   }
+  nextPhase(){
+    this.setState({ phase: (this.state.phase+1)%3 });
+  }
+  getControllTitle(){
+    return (this.state.phase === 0 ? 'Placement Phase' : ( this.state.phase ===1 ? 'Attack Phase': 'Movement Phase' ) );
+  }
+  getSubmitButton(){
+    return (<button onClick={(this.state.phase === 0 ? this.nextPhase : ( this.state.phase ===1 ? this.nextPhase: this.nextPhase ) )} > {(this.state.phase === 0 ? 'Deploy' : ( this.state.phase ===1 ? 'Launch Attack': 'Move Reinforcements' ) )}</button>);
+  }
   render() {
     //console.log(this.state.hilite);
+
     return (
       <div className="App">
-        <div>
-        <Controller
-                    type={this.state.type}
-                    phase={this.state.phase}
-                    selectFrom={this.selectFrom}
-                    selectTo={this.selectTo}
-                    />
-      </div>
-        <div>
-          <p> From: {this.state.from} To: {this.state.to}</p>
-        </div>
+        <table width="100%" height="1000">
+          <td width="20%" height="100%">
+            <tr>
+              {this.getControllTitle()}
+            </tr>
+            <tr>
+              <input type={'range'}/>
+            </tr>
+            <tr>
+              <button onClick={console.log}>Add</button>
+            </tr>
+            <tr>
+              <p className={'scrollBox'}> Here is where we can put a list of all of the attacks Here is where we can put a list of all of the attacks Here is where we can put a list of all of the attacks Here is where we can put a list of all of the attacks</p>
+              </tr>
+            <tr>
+              {this.getSubmitButton()}
+            </tr>
+          </td>
+          <td width="80%" height="100%">
         <Board select={this.selectCountry} board={this.getTestBoard()} makeSelect={this.makeSelect} selections={this.state.hilite} />
-
+          </td>
+        </table>
       </div>
 
   );
   }
 
+  /**
+   * <div className="App">
+   <table width="100%" height="1000">
+   <td width="20%" height="100%">
+   <div>
+   <Controller
+   type={this.state.type}
+   phase={this.state.phase}
+   selectFrom={this.selectFrom}
+   selectTo={this.selectTo}
+   />
+   </div>
+
+   <div>
+   <p> From: {this.state.from} To: {this.state.to}</p>
+   </div>
+   </td>
+   <td width="80%" height="100%">
+   <Board select={this.selectCountry} board={this.getTestBoard()} makeSelect={this.makeSelect} selections={this.state.hilite} />
+   </td>
+   </table>
+   </div>
+   *
+   *
+   */
 
 }
 
