89a6524ad8675dd97a3c521624922d664333158b
Luke Burford <lburford@rams.colostate.edu>
2019-04-04 14:38:04 -0600

    Implementing status and public phase driver functions

--- risk-chain/ethereum/contracts/RISK.sol
+++ risk-chain/ethereum/contracts/RISK.sol
@@ -4,10 +4,11 @@ contract RISK {
     // Data Types
 
     enum armyType {Solider, Horse, Cannon}
+    enum Status {Waiting, Placing, Attacking, Transferring, Dead}
 
     struct Player {
         string name;
-        uint status; // int that defines player alive/dead, current turn, and current phase. TODO: exact implimentation TBD
+        Status status;
         uint index;
         uint armyIncome;
     }
@@ -31,6 +32,7 @@ contract RISK {
     mapping(address => Player) Players;
     mapping(uint => Continent) Continents;
     mapping(uint => Region) Regions;
+    address[] PlayerAddrs;
     uint[] bonus = [3,7,2,5,5,2];
     uint[] numRegions = [6,12,4,7,9,4];
     uint[] numAdjList = [6,4,3,2,3,5,5,4,4,5,6,5,3,6,2,5,4,3,2,3,3,4,4,3,5,4,5,6,4,3,4,3,4,4,4,5,4,3,2,4,3,3];
@@ -62,10 +64,61 @@ contract RISK {
         }
     }
 
-    // Functions
+    // Public Phase Functions
 
-    function PlaceTroops(uint newArmies, uint loc) internal returns(bool success) {
-        // Needs to have a require for checking correct turn phase
+    /** Drives all the requested armies placements as one block
+    *   precondition: the length of newArmies must be the length of locations
+    *   precondition: the player must be in the Placing status to place troops.
+    **/
+    function PlaceTroopsDriver(uint[] Locations, uint[] newArmies) public returns(bool success) {
+        success = false; // Only return true if the function has finished
+        assert(newArmies.length == Locations.length);
+        require(Players[msg.sender].status == Status.Placing, "You can't place armies right now!");
+        for(uint i=0; i < newArmies.length; ++i) {
+            if(!PlaceTroops(Locations[i], newArmies[i]))
+                return false;
+        }
+        Players[msg.sender].status = Status.Attacking;
+        return true;
+    }
+
+    /** Drives all the requested attacks as one block
+    *   precondition: the length of numArmies must be the length of toLoc & fromLoc
+    *   precondition: the player must be in the Attacking status to attack.
+    **/
+    function AttackDriver(uint[] input, uint seed) public returns(bool success) {
+        success = false; // Only return true if the function has finished
+        require(Players[msg.sender].status == Status.Attacking, "You can't attack right now!");
+        for(uint i=0; i < input.length; i+=3) {
+            Attack(input[i], input[i+1], input[i+2], seed);
+            // if(!Attack(input[i], input[i+1], input[i+2], seed))
+            //     return false;
+        }
+        Players[msg.sender].status = Status.Transferring;
+        return true;
+    }
+
+    /** Drives all the requested transfers as one block
+    *   precondition: the length of numArmies must be the length of toLoc & fromLoc
+    *   precondition: the player must be in the Transfering status to transfer.
+    **/
+    function TrfArmiesDriver(uint[] input) public returns(bool success) {
+        success = false; // Only return true if the function has finished
+        require(Players[msg.sender].status == Status.Transferring, "You can't transfer armies right now!");
+        for(uint i=0; i < input.length; i+=3) {
+            if(!TrfArmies(input[i], input[i+1], input[i+2]))
+                return false;
+        }
+        Players[msg.sender].status = Status.Waiting; // current players turn is now over.
+        uint nextPlayer = Players[msg.sender].index + 1 % (PlayerAddrs.length - 1);
+        Players[PlayerAddrs[nextPlayer]].status = Status.Placing; // sets the next player to the start of their turn (as Placing)
+        return true;
+    }
+
+    // Internal Phase Functions
+
+    function PlaceTroops(uint loc, uint newArmies) internal returns(bool success) {
+        success = false; // Only return true if the function has finished
         require(Regions[loc].owner == msg.sender, "You do not own this region");
         require(newArmies > 0, "Must choose amount of armies to place");
         require(Players[msg.sender].armyIncome >= newArmies, "Not enough army surplus remaining to place these armies");
@@ -76,12 +129,12 @@ contract RISK {
     }
 
     function Attack(uint fromLoc, uint toLoc, uint numArmies, uint seed) internal returns(bool success) {
+        success = false; // Only return true if the function has finished
         Region fromRegion = Regions[fromLoc];
         Region toRegion = Regions[toLoc];
         Player attacker = Players[msg.sender];
         Player defender = Players[toRegion.owner];
 
-        // Needs to have a require for checking correct turn phase
         require(fromRegion.owner == msg.sender, "You can't attack from that region, you do not own it.");
         require(toRegion.owner != msg.sender, "Can only transfer troops during the transfer phase.");
         require(isAdjacent(fromRegion, toLoc), "You must attack regions that are adjacent.");
@@ -132,6 +185,7 @@ contract RISK {
                 attackerWins = true;
         }
         if(attackerWins) {
+            //TODO impliment checking if defender is still alive and update it appropriatly
             toRegion.numArmies = atckArmy; // remaining attacker armies are transfered to the toRegion
             toRegion.owner = msg.sender; // ownership is transfered to the attacker
         }
@@ -143,12 +197,12 @@ contract RISK {
         return true;
     }
 
-    function TrfTroops(uint numArmies, uint fromLoc, uint toLoc) internal returns(bool success) {
+    function TrfArmies(uint numArmies, uint fromLoc, uint toLoc) internal returns(bool success) {
+        success = false; // Only return true if the function has finished
         Region fromRegion = Regions[fromLoc];
         Region toRegion = Regions[toLoc];
         Player player = Players[msg.sender];
 
-        // Needs to have a require for checking correct turn phase
         require(fromRegion.owner == msg.sender && toRegion.owner == msg.sender, "You must own both regions to transfer");
         require(isAdjacent(fromRegion, toLoc), "You can only transfer to regions that are adjacent.");
         require(fromRegion.numArmies >= numArmies , "Trying to transfer with more armies that are in the region."); //TODO: make that error message better.
